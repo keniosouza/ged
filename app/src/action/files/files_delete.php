4bd337ca10db02efdf1aa8318b621861b1646772
Kenio de Souza <kenio_souza@hotmail.com>
2025-07-14 19:25:14 -0300

    Ajustando rotina de exclusão de arquivo

--- app/src/action/files/files_delete.php
+++ app/src/action/files/files_delete.php
@@ -16,6 +16,8 @@ $result = null;
 $FilesValidate->setFileId((int) filter_input(INPUT_POST, 'file_id', FILTER_SANITIZE_SPECIAL_CHARS));
 $FilesValidate->setUserId((int) $_SESSION['MY_SAAS_USER']['user_id']);
 
+print_r($_SESSION['MY_SAAS_USER']);
+
 /** Verifico a existência de erros */
 if (!empty($FilesValidate->getErrors())) {
 

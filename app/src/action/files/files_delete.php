2327c86fb7bb594d81d009eae168469381a93bc4
Kenio de Souza <kenio_souza@hotmail.com>
2025-07-14 19:57:59 -0300

    Ajustando rotina de exclusão de arquivo

--- app/src/action/files/files_delete.php
+++ app/src/action/files/files_delete.php
@@ -30,10 +30,10 @@ if (!empty($FilesValidate->getErrors())) {
         $result = [
 
             'code' => 200,
-            'data' => 'Perfil Atualizado',
+            'data' => 'Arquivo removido',
             'toast' => [
                 [
-                    'background' => 'alert-success',
+                    'background' => 'bg-warning ',
                     'data' => '<i class="bi bi-trash me-1"></i>Registro removido'
                 ]
             ],

6b5cf41d0d7c6ea9ee53056900afc9bdb1a6ea93
Kenio de Souza <kenio_souza@hotmail.com>
2025-07-14 18:02:11 -0300

    Habilitando hash de renovação de senha

--- app/src/action/users/users_login.php
+++ app/src/action/users/users_login.php
@@ -52,7 +52,8 @@ try {
                 if( (!empty($UsersGetByEmailResult->password_temp)) && (empty($UsersGetByEmailResult->password))){
 
                     /** Gera o hash */
-                    $urlNewPwd = $Main->getUrlApp().'new-password/'.$Main->encryptData($UsersGetByEmailResult->email.'*'.$UsersGetByEmailResult->user_id);
+                    $urlNewPwd = 'new-password/'.$Main->encryptData($UsersGetByEmailResult->email.'*'.$UsersGetByEmailResult->user_id);
+
 
                     // Result
                     $result = [
